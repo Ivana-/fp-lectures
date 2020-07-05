8640976e09e9269d8057514af9d17ccf0593b308
Andrey <Ivana_2004@mail.ru>
2020-07-06 00:33:37 +0300

    max rec depth calc - fp way

--- fp-1.cpp
+++ fp-1.cpp
@@ -20,7 +20,7 @@ ull f(int n) {
     ull s = 0;
     for (int i=0; i<=n; i++) s += m[i];
     return s;
-}   
+}
 
 
 
@@ -44,7 +44,7 @@ ull d(int b, int e) {
 
 int main() {
     for (int i=0; i<N; i++) m[i] = i;
-    
+
     int n = 1E1; //1E7;
     std::cout << f(n) << "\n";
     std::cout << g(n) << "\n";
@@ -62,7 +62,7 @@ ull d(int b, int e, int l) {
     int c = (b + e)/2;
     int nxt_l = l + 1;
     max_l = std::max(max_l, nxt_l);
-    
+
     return (b==e) ? m[b] : d(b, c, nxt_l) + d(c+1, e, nxt_l);
 }
 
@@ -71,6 +71,28 @@ ull d(int b, int e, int l) {
     std::cout << max_l << "\n";
 */
 
+
+// Max recursion depth calculation - functional way
+/*
+typedef std::pair <ull, int> ull_int;
+
+ull_int dd(int b, int e, int l) {
+    int c = (b + e)/2;
+    int nxt_l = l + 1;
+
+    if (b==e)
+        return std::make_pair(m[b], nxt_l);
+    else {
+        ull_int r1 = dd(b, c, nxt_l);
+        ull_int r2 = dd(c+1, e, nxt_l);
+        return std::make_pair(r1.first + r2.first, std::max(r1.second, r2.second));
+    }
+}
+
+    ull_int r = dd(0, n, 0);
+    std::cout << r.first << "\n";
+    std::cout << r.second << "\n";
+*/
 
 
 // Haskel code
